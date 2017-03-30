9618bb33adc40490c250ef45d3492656ad2f7fb9
JackZ <emailjiong@126.com>
2017-03-30 18:51:11 +0800

    First Version of File_Reqst

--- README.md
+++ README.md
@@ -1,2 +1,103 @@
 # wxapp-file_reqst
-A module to manage files in wxapp.
+A module to manage files and offer a request queue in wxapp.
+
+
+
+## Import
+
+```javascript
+var file_reqst = require('/path/to/file_reqst.js');
+```
+
+
+
+## Usage
+
+1. `getRemainSpace()`
+
+   This will return the remaining space of local file storage.
+
+   ```javascript
+   Page({
+     ...
+     someMethod: function(){
+       ...
+     	remainingSpace = file_reqst.getRemainSpace();
+     	...
+     }
+   })
+   ```
+
+   ​
+
+2. `downloadAndSave(obj)`
+
+   Required object format:
+
+   | key         | type       | required | value                                    |
+   | ----------- | ---------- | -------- | ---------------------------------------- |
+   | `url`       | `String`   | `true`   | the url to download resources.           |
+   | `name`      | `String`   | `false`  | rename the file.(default is download name) |
+   | `collision` | `String`   | `false`  | `'both'` or `'older'` or `'newer'`.(default is `'newer'`) |
+   | `csname`    | `String`   | `false`  | when collision is set to `'both'`, file will be renamed to csname. |
+   | `header`    | `Object`   | `false`  | request header.                          |
+   | `success`   | `Function` | `false`  | the callback when successfully saved.    |
+   | `fail`      | `Function` | `false`  | the callback when save failed.           |
+   | `complete`  | `Function` | `false`  | the callback when save completed.        |
+
+   P.S. `csname` is required when collision is set to `'both'`.
+
+   ```javascript
+   ...
+   file_reqst({
+     url: 'https://example.com/file/path',
+     name: 'rename.ext'
+   });
+   ...
+   ```
+
+3. `getAllFiles()`
+
+   This will return all the files managed by file_reqst.js.
+
+4. `getFile(name, callback)`
+
+   This will return the file path corresponding to the name. And the callback will receive an object like the following one:
+
+   ```javascript
+   {
+     filePath: 'path/to/your/file',
+     createTime: 1490870527394,
+     size: 233
+   }
+   ```
+
+   P.S. An Exception will be thrown when file doesn't exist.
+
+5. `removeFile(name, callback)`
+
+   The callback will receive an object like the following one:
+
+   ```javascript
+   {
+     message: 'fail',
+     info: {something provided by wxapp}
+   }
+   {
+     message: 'ok',
+     info: {something provided by wxapp}
+   }
+   ```
+
+6. `clearFile(callback)`
+
+   The callback will receive an object like `removeFile` received.
+
+
+
+## Support
+
+[ZLab](https://jackz.cn)
+
+[z-jack(GitHub)](https://github.com/z-jack)
+
---
+++ file_reqst.js
@@ -0,0 +1,256 @@
+if (typeof (String.prototype.trim) === "undefined") {
+    String.prototype.trim = function () {
+        return String(this).replace(/^\s+|\s+$/g, '');
+    };
+}
+
+fileStatus = {
+    remain: 10485760,
+    fileMap: {},
+    ready: false
+};
+
+queue = [];
+
+function getRemainSpace() {
+    if (!fileStatus.ready) {
+        initStatus();
+    }
+    return fileStatus.remain;
+}
+
+/*
+    required object format:
+    |key        |type       |required   |value
+    |===========|===========|===========|================================================================
+    |url        |String     |true       |the url to download resources.
+    |name       |String     |false      |rename the file.(default is download name)
+    |collision  |String     |false      |'both' or 'older' or 'newer'.(default is 'newer');
+    |csname     |String     |false      |when collision is set to 'both', file will be renamed to csname.
+    |header     |Object     |false      |request header.
+    |success    |Function   |false      |the callback when successfully saved.
+    |fail       |Function   |false      |the callback when save failed.
+    |complete   |Function   |false      |the callback when save completed.
+    
+    P.S. csname is required when collision is set to 'both' mode.
+*/
+function downloadAndSave(obj) {
+    if (!fileStatus.ready) {
+        initStatus();
+    }
+    if (!obj || typeof obj != 'object' || !(obj.url)) throw ('Error: Object format error.');
+    if (obj.collision == 'both' && (typeof obj.csname != 'string' || !(obj.csname))) throw ('Error: csname is required.');
+    if (!obj.collision || typeof obj.collision != 'string') obj.collision = 'newer';
+    if (typeof obj.name != 'string') obj.name = undefined;
+
+    const downloadExceed = 'downloadFile:fail exceed max download connection count 10';
+    const saveExceed = 'saveFile:fail the maximum size of the file storage limit is exceeded';
+
+    var name = nameParser(obj.name || obj.url);
+    if (fileStatus.fileMap[name])
+        switch (obj.collision) {
+            case 'both':
+                if (fileStatus.fileMap[nameParser(obj.csname)]) throw ('Error: File named ' + nameParser(obj.csname) + ' already exists.');
+                name = nameParser(obj.csname);
+                break;
+            case 'newer':
+                var waiting = true,
+                    busy = false;
+                while (waiting) {
+                    if (!busy) {
+                        busy = true;
+                        removeFile(name, () => {
+                            waiting = false;
+                        })
+                    }
+                }
+                break;
+            case 'older':
+                return;
+        }
+    if (!name) throw ('Error: Name cannot be empty.');
+    wx.downloadFile({
+        url: obj.url,
+        header: obj.header,
+        success: (res) => {
+            saveFile(res);
+            if (queue.length) {
+                downloadAndSave(queue.shift());
+            }
+        },
+        fail: (e) => {
+            if (e.errMsg == downloadExceed) {
+                queue.push(obj);
+            } else {
+                obj.fail && obj.fail(e);
+            }
+        }
+    })
+}
+
+function saveFile(res) {
+    wx.saveFile({
+        tempFilePath: res.tempFilePath,
+        success: (bk) => {
+            wx.getSavedFileInfo({
+                filePath: bk.filePath,
+                success: (alres) => {
+                    fileStatus.fileMap[name] = {
+                        filePath: bk.filePath,
+                        createTime: alres.createTime,
+                        size: alres.size
+                    }
+                }
+            })
+            obj.success && obj.success(bk);
+        },
+        fail: (e) => {
+            if (e.errMsg == saveExceed) {
+                if (fileStatus.remain >= 10485760) {
+                    obj.fail && obj.fail({
+                        errMsg: 'saveFile:fail file too large to save'
+                    })
+                } else {
+                    var min = {
+                        createTime: Date.now()
+                    }
+                    for (var i in fileStatus.fileMap) {
+                        if (fileStatus.fileMap[i].createTime < min.createTime) {
+                            min = fileStatus.fileMap[i];
+                            min.name = i;
+                        }
+                    }
+                    removeFile(min.name, () => {
+                        saveFile(res);
+                    })
+                }
+            } else {
+                obj.fail && obj.fail(e);
+            }
+        },
+        complete: (e) => {
+            obj.complete && obj.complete(e);
+        }
+    })
+}
+
+function getAllFiles() {
+    return fileStatus.fileMap;
+}
+
+function getFile(name, cbk) {
+    if (!fileStatus.ready) {
+        initStatus();
+    }
+    if (typeof cbk != 'function') {
+        cbk = (() => {});
+    }
+    if (!name || typeof name != 'string') throw ('Error: File name error.');
+
+    if (fileStatus.fileMap[name]) {
+        cbk(fileStatus.fileMap[name]);
+        return fileStatus.fileMap[name].filePath;
+    } else {
+        throw ("Error: File doesn't exist.");
+    }
+}
+
+function removeFile(name, cbk) {
+    if (!fileStatus.ready) {
+        initStatus();
+    }
+    if (typeof cbk != 'function') {
+        cbk = (() => {});
+    }
+    if (!name || typeof name != 'string') throw ('Error: File name error.');
+
+    if (fileStatus.fileMap[name]) {
+        wx.removeSavedFile({
+            filePath: fileStatus.fileMap[name].filePath,
+            success: (e) => {
+                fileStatus.remain += fileStatus.fileMap[name].size;
+                delete fileStatus.fileMap[name];
+                saveStatus();
+                cbk({
+                    message: 'ok',
+                    info: e
+                });
+            },
+            fail: (e) => cbk({
+                message: 'fail',
+                info: e
+            })
+        });
+    }
+}
+
+function clearFile(cbk) {
+    if (!fileStatus.ready) {
+        initStatus();
+    }
+    if (typeof cbk != 'function') {
+        cbk = (() => {});
+    }
+
+    for (var i in fileStatus.fileMap) {
+        wx.removeSavedFile({
+            filePath: fileStatus.fileMap[i].filePath,
+            fail: (e) => {
+                cbk({
+                    message: 'fail',
+                    info: {
+                        name: i,
+                        filePath: fileStatus.fileMap[i].filePath,
+                        detail: e.errMsg
+                    }
+                })
+            }
+        })
+    }
+
+    cbk({
+        message: 'ok',
+        info: null
+    })
+}
+
+function initStatus() {
+    fileStatus = wx.getStorageSync('file_reqst_status') || fileStatus;
+    var busy = false;
+    while (!fileStatus.ready) {
+        if (!busy) {
+            busy = true;
+            wx.getSavedFileList({
+                success: function (res) {
+                    for (var i in res.fileList) {
+                        fileStatus.fileMap[nameParser(res.fileList[i].filePath)] = res.fileList[i];
+                        fileStatus.remain -= res.fileList[i].size;
+                    }
+                    fileStatus.ready = true;
+                },
+                fail: () => {
+                    fileStatus.ready = true;
+                }
+            })
+        }
+    }
+    saveStatus();
+}
+
+function saveStatus() {
+    wx.setStorageSync('file_reqst_status', fileStatus);
+}
+
+function nameParser(name) {
+    name = name.split('/');
+    return name[name.length - 1].trim();
+}
+
+module.exports = {
+    getRemainSpace: getRemainSpace,
+    downloadAndSave: downloadAndSave,
+    getAllFiles: getAllFiles,
+    getFile: getFile,
+    removeFile: removeFile,
+    clearFile: clearFile
+}
